1032dbd84b70b6841a483e2690307e9b4eb15fc8
Phil Pearl (Lobbes) <phil@perkpartners.com>
2015-07-09 11:01:15 -0400

    - imported code

---
+++ etc/sample.sh
@@ -0,0 +1,5 @@
+# this should include the zimbra perl library directory and the lib directory
+# associated with the ZCS::API
+export PATH=/path/to/zcs/api/bin:$PATH
+export PERL5LIB=/opt/zimbra/zimbramon/lib:/path/to/zcs/api/lib
+export ZCS_CONFIG=/path/to/zcs/api/config/file/<site>.json
